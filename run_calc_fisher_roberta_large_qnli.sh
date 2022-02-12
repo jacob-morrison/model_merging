c22bc075a82d98e18bd53f997f8b35fd08cfdd79
Jacob Morrison <jacob1morrison@gmail.com>
2022-02-11 19:26:00 -0800

    add qnli

---
+++ run_calc_fisher_roberta_large_qnli.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#$ -l rt_AF=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -cwd
+
+## >>> conda init >>>
+
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+
+if [ $? -eq 0 ]; then
+    \eval "$__conda_setup"
+else
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="$PATH:$HOME/anaconda3/bin"
+    fi
+fi
+unset __conda_setup
+
+conda activate merge-models
+nvidia-smi
+python3 compute_fisher.py --model howey/roberta-large-qnli --fisher_path ../fishers/qnli-1.hdf5 --glue_task qnli 
