dfe4b00d43836d5a0f713191676c05b459925d25
Jacob Morrison <jacob1morrison@gmail.com>
2022-02-21 19:29:10 -0800

    test roberta script

---
+++ eval_roberta_mnli.py
@@ -0,0 +1,21 @@
+# from transformers import RobertaTokenizer, RobertaModel, RobertaForSequenceClassification
+# import torch
+# from datasets import load_dataset
+
+# model = RobertaForSequenceClassification.from_pretrained('roberta-large-mnli', num_labels=3)
+# tokenizer = RobertaTokenizer.from_pretrained("roberta-large-mnli") # check if this is the right tokenizer
+# dataset = load_dataset('glue', 'mnli_matched')
+# print(dataset['validation'][0])
+# for premise, hypothesis, label, idx in dataset['validation']:
+#     inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+#     labels = torch.tensor([label]).unsqueeze(0)  # Batch size 1
+#     outputs = model(**inputs, labels=labels)
+#     loss = outputs.loss
+#     logits = outputs.logits
+#     break
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("roberta-large-mnli")
+model = AutoModelForSequenceClassification.from_pretrained("roberta-large-mnli")
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
