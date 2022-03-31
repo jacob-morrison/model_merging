46f54d806eb889f65f6070bc97a99c56404b82a1
Jacob Morrison <jacob1morrison@gmail.com>
2022-03-30 17:47:38 -0700

    add bash script

---
+++ run_test_custom_model.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$ -l rt_AF=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -cwd
+
+## >>> conda init >>>
+
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+
+if [ $? -eq 0 ]; then
+    \eval "$__conda_setup"
+else
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="$PATH:$HOME/anaconda3/bin"
+    fi
+fi
+unset __conda_setup
+
+conda activate merge-models
+module load cuda/11.2
+module load cudnn/8.1
+nvidia-smi
+python3 test_custom_model.py 
