e8a70ca5d084a878fd4eacc629991b2ee54690f6
Jacob Morrison <jacob1morrison@gmail.com>
2022-03-30 21:44:39 -0700

    test

--- test_custom_model.py
+++ test_custom_model.py
@@ -1,14 +1,15 @@
 from transformers import RobertaConfig, RobertaModel, ViTFeatureExtractor, ViTModel
 
 configuration = RobertaConfig()
-test_model = RobertaModel.from_pretrained(
+roberta_model = RobertaModel.from_pretrained(
     '/home/acd13578qu/scratch/roberta_actual/checkpoints/checkpoint_best.pt',
     config=configuration)
+vit_model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
 
 bert_params = []
 bert_shapes = []
 bert_total_params = 0
-for name, param in test_model.named_parameters():
+for name, param in roberta_model.named_parameters():
     if param.requires_grad:
         print(str(name))
         start = 1
@@ -17,3 +18,24 @@ for name, param in test_model.named_parameters():
             start *= elem
         bert_total_params += start
         bert_params.append(name)
+
+roberta_params = []
+roberta_shapes = []
+roberta_total_params = 0
+for name, param in vit_model.named_parameters():
+    if param.requires_grad:
+        print(str(name))
+        start = 1
+        roberta_shapes.append(param.data.size())
+        for elem in list(param.data.size()):
+            start *= elem
+        roberta_total_params += start
+        roberta_params.append(name)
+
+for bert_param, roberta_param, bert_shape, roberta_shape in zip(bert_params, roberta_params, bert_shapes, roberta_shapes):
+    if bert_shape != roberta_shape:
+        print('Mismatch!!')
+        print(bert_shape)
+        print(bert_param)
+        print(roberta_shape)
+        print(roberta_param)
