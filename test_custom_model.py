f8b6f96f516b02e37c96de98a1a069586f80d44c
Jacob Morrison <jacob1morrison@gmail.com>
2022-05-19 22:43:48 -0700

    test

--- test_custom_model.py
+++ test_custom_model.py
@@ -18,9 +18,9 @@ vit_model = TFViTModel.from_pretrained('google/vit-base-patch16-224-in21k', from
 vit_layers = vit_model.layers[0]
 
 print('roberta layers:')
-print(roberta_layers.encoder.summary())
+print(roberta_layers.encoder.trainable_variables.shape)
 print(len(roberta_layers.encoder.layer))
-print(vit_layers.encoder.summary())
+print(vit_layers.encoder.trainable_variables.shape)
 print(len(vit_layers.encoder.layer))
 # pprint(dir(roberta_model))
 # pprint(dir(roberta_layers))
